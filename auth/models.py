5f0582ee0990c1df101f1fb006529744c84e9679
wishket-ysj <sjyoon@wishket.com>
2024-07-21 01:08:40 +0900

    feat: email unique=True

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Account(AbstractUser):
+    email = models.EmailField(_("email address"), blank=True, unique=True)
