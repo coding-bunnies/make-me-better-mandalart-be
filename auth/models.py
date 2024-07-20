fd34aa7202c5fab5ece538da743238fefe450b3a
blingblin-g <luvbliny@gmail.com>
2024-07-21 02:16:06 +0900

    refactor: 불필요한 주석 삭제

--- auth/models.py
+++ auth/models.py
@@ -3,6 +3,5 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
 class Account(AbstractUser):
     email = models.EmailField(_("email address"), blank=True, unique=True)
