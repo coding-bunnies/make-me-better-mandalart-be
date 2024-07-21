30efe6b6987a3709d1ea30fcf35426858fbebc06
blingblin-g <luvbliny@gmail.com>
2024-07-22 02:09:43 +0900

    fix: type hint

--- core/tests.py
+++ core/tests.py
@@ -1,11 +1,16 @@
+import typing
+
 from rest_framework.test import APITestCase
 
 from core.factories import AccountFactory
 
+if typing:
+    from auth.models import Account
+
 
 # Create your tests here.
 class BaseAPITestCase(APITestCase):
-    def get_authenticated_user(self, user=None) -> "AccountFactory":
+    def get_authenticated_user(self, user=None) -> "Account":
         if not user:
             user = AccountFactory()
         self.client.force_authenticate(user=user)
